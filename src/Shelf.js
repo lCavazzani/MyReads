4bb365084aa1972997a234bf5dc7441be8c56697
leonardoCavazzani <cavazzanileonardo@gmail.com>
2017-11-10 12:55:09 -0200

    pretty books

--- src/Shelf.js
+++ src/Shelf.js
@@ -17,10 +17,23 @@ class Shelf extends Component{
               {console.log(this.props.books)}
 
                  {this.props.books.filter(books => books.shelf === this.props.type_filter).map(book=>
-                                      <li key={book.id} className='contact-list-item'>
-                                              <div>
-                                                  <p>{book.title}</p>
-                                              </div>
+                                      <li key={book.id} >
+                                               <div className="book">
+                                                  <div className="book-top">
+                                                    <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                                                    <div className="book-shelf-changer">
+                                                      <select>
+                                                        <option value="none" disabled>Move to...</option>
+                                                        <option value="currentlyReading">Currently Reading</option>
+                                                        <option value="wantToRead">Want to Read</option>
+                                                        <option value="read">Read</option>
+                                                        <option value="none">None</option>
+                                                      </select>
+                                                    </div>
+                                                  </div>
+                                                  <div className="book-title">{book.name}</div>
+                                                  <div className="book-authors">{book.authors.join(", ")}</div>
+                                                </div>
                                           </li>
                                          )}
               </ol>
